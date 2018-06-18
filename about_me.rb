fbd5b2522a13fbc683205ea42d01162262f9694b
Sara Beth Ribot <sbribot@gmail.com>
2018-06-18 22:59:20 +0000

    automatically backed up by learn

---
+++ about_me.rb
@@ -0,0 +1,9 @@
+def about_me
+  puts "My name is Chen"
+  puts "I'm from Taiwan"
+  puts "I went to Otay Ranch High School in Chula Vista, San Diego"
+  puts "I have a dog name Doggo"
+  puts "He accidentally pooped in my room twice"
+  puts "Therefore, I never let him in my room by himself"
+  puts "hashtag trust issues"
+end
