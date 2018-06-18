9dd98f46092e8334dbe83a55c2fbc555fba94b7f
Sara Beth Ribot <sbribot@gmail.com>
2018-06-18 22:55:47 +0000

    automatically backed up by learn

---
+++ about_me.rb
@@ -0,0 +1,16 @@
+def about_me
+  puts "My name is Chen"
+  puts "I am from Taiwan"
+  puts "I am 23 years old"
+  puts "My favorite ice cream flavor is pecan praline"
+end
+
+def about_SB
+  puts "Sara Beth is a MTLD"
+  puts "Sara Beth has green eyes"
+  puts "Sara Beth is going to work at Google"
+  puts "Sara Beth is also left handed"
+end 
+
+about_me
+about_SB
