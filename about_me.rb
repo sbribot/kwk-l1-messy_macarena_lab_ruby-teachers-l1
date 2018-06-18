d813f5f50845699f5aa5a2f48e678068f295def9
Sara Beth Ribot <sbribot@gmail.com>
2018-06-18 23:03:43 +0000

    automatically backed up by learn

--- about_me.rb
+++ about_me.rb
@@ -1,11 +1,11 @@
-def about_me
+def about_me #def starts a method 
   puts "My name is Chen"
   puts "I'm from Taiwan"
   puts "I went to Otay Ranch High School in Chula Vista, San Diego"
   puts "I have a dog name Doggo"
   puts "He accidentally pooped in my room twice"
   puts "Therefore, I never let him in my room by himself"
-  puts "hashtag trust issues"
+  puts "hashtag trust issues" 
 end
 
 about_me
